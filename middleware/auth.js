2b49b7fcf441bf447cc48b42d5d25dd34c88fe01
pop-catalin <catalinpop98@gmail.com>
2020-11-07 22:49:53 +0200

    profile routes backend

--- middleware/auth.js
+++ middleware/auth.js
@@ -13,7 +13,6 @@ module.exports = function (req, res, next) {
 	// verify token
 	try {
 		const decoded = jwt.verify(token, config.get('jwtSecret'));
-		console.log(decoded);
 		req.user = decoded.user;
 		next();
 	} catch (err) {
