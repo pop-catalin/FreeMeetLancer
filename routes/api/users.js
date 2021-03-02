a43acca5bc93c503a403b81d637ecf31ff9878c2
pop-catalin <catalinpop98@gmail.com>
2021-03-02 23:12:25 +0200

    removed gravatar, added random avatars

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
 const { check, validationResult } = require('express-validator/check');
+//random avatars
+const avatars = [
+	'https://i.imgur.com/GxDX8cV.png',
+	'https://i.imgur.com/KaEqmbr.png',
+	'https://i.imgur.com/CUw0mtw.png',
+	'https://i.imgur.com/CDxtHiC.png',
+	'https://i.imgur.com/JbPoJsN.png',
+	'https://i.imgur.com/zRz7MZl.png',
+	'https://i.imgur.com/XEiyLVL.png',
+	'https://i.imgur.com/8zCGikH.png',
+	'https://i.imgur.com/QWe4qbG.png',
+	'https://i.imgur.com/XmKkUxR.png',
+];
 
 const User = require('../../models/User');
 
@@ -38,11 +50,7 @@ router.post(
 					.json({ errors: [{ msg: 'User already exists' }] });
 			}
 
-			const avatar = gravatar.url(email, {
-				s: '200',
-				r: 'pg',
-				d: 'mm',
-			});
+			const avatar = avatars[Math.floor(Math.random() * avatars.length)];
 
 			user = new User({
 				name,
